1bf0f96d536016f3a5260345c2aec198f4965e43
EnverMT <email>
2026-04-17 11:00:25 +0500

    anagram

--- Leetcode/Array/ValidAnagram.py
+++ Leetcode/Array/ValidAnagram.py
@@ -1,3 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        pass
+        if len(s) != len(t):
+            return False
+
+        storeS = dict()
+        storeT = dict()
+        for i in range(len(s)):
+            storeS[s[i]] = storeS.get(s[i], 0) + 1
+            storeT[t[i]] = storeT.get(t[i], 0) + 1
+
+        return storeS == storeT
